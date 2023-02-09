5b9f4f28347db993316181a39796cb845fa6f0ed
Lapo0 <lapo.gini.01@gmail.com>
2023-02-09 15:52:07 +0100

    FEAT consol.log ex

---
+++ js/app.js
@@ -0,0 +1,39 @@
+// CREARE UN PULSANTE CON EVENTO CLICK CHE FACCIA APPARIRE LA GRIGLIA
+
+// Stabilire lato griglia e celle totali
+
+let latoGriglia = 10
+let celleTotali = latoGriglia ** 2
+console.log(latoGriglia, celleTotali)
+
+// Selezionare la griglia dal DOM
+const gridEl = document.querySelector ('.griglia')
+console.log(gridEl)
+
+// Creare ciclo per mettere le celle in base a quante ne sono state selezionate
+for (let i = 0; i < celleTotali; i++) {
+    let numGrid = i + 1
+    console.log(numGrid)
+
+    const cellaString = `<div class="cella" style="width: calc(100% / ${ latoGriglia })"> ${ numGrid} </div>`
+
+    // Concatenare la cella all'elemento griglia
+    gridEl.innerHTML += cellaString
+}
+
+// Prendere i div creati con classe .cella
+const celleElements = document.querySelectorAll('.cella')
+console.log(celleElements)
+
+// Creare un ciclo per andare a stampare il numero corrispondente a ognuna
+for (let i = 0; i < celleElements.length; i++) {
+    let numCell = i + 1
+    console.log(numCell)
+
+    const cella = celleElements[i]
+
+    // Assegnargli il numero corretto al click
+    cella.addEventListener('click', function() {
+        console.log(numCell)
+    })
+}
